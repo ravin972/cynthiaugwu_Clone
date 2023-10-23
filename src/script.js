2c8bb2c27ce7b3ea1202c4526de67d9f21488994
Ravinder Pandey <pandeyravinder078@gmail.com>
2023-10-24 02:03:50 +0530

    js update

--- src/script.js
+++ src/script.js
@@ -1,11 +1,39 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+  el: document.querySelector("#main"),
+  smooth: true,
 });
 
-const circleMouseFollower = () => {
-    window.addEventListener('mousemove', (e) => {
-        document.querySelector(".minicircle").style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+const firstPageAnimation = () => {
+  const tl = gsap.timeline();
+
+  tl.from(".navbar", {
+    y: -10,
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+    .to(".boundingelem", {
+      y: 0,
+      ease: Expo.easeInOut,
+      duration: 2,
+      delay: -1,
+      stagger: 0.2,
     })
-}
+    .from(".hero-footer", {
+      y: -10,
+      ease: Expo.easeInOut,
+      duration: 1.5,
+      delay: -1,
+      opacity: 0,
+    });
+};
+
+const circleMouseFollower = () => {
+  window.addEventListener("mousemove", (e) => {
+    document.querySelector(
+      ".minicircle"
+    ).style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+  });
+};
 circleMouseFollower();
+firstPageAnimation();
